8f390c8b1f7cbbae3163c1f23822d9cc55d583eb
Veronica Ludemann <veronicaludemann67@gmail.com>
2022-07-08 18:57:46 -0300

    varios
    varios

--- src/main/java/com/backend/Back/Security/MainSecurity.java
+++ src/main/java/com/backend/Back/Security/MainSecurity.java
@@ -9,7 +9,7 @@ import com.backend.Back.Security.jwt.JwtTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
@@ -19,7 +19,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
+
 
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
