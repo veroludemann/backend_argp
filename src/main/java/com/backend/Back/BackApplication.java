b701defa9b848931bf1426a08e98c23764339ef8
Veronica Ludemann <veronicaludemann67@gmail.com>
2022-07-27 08:55:39 -0300

    sitioweb

--- src/main/java/com/backend/Back/BackApplication.java
+++ src/main/java/com/backend/Back/BackApplication.java
@@ -19,7 +19,7 @@ public class BackApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://proyectoangular-30a1e.web.app"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
