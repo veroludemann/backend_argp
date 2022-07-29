0b9148701f08c0d573eafbb91196616affb4b549
Veronica Ludemann <veronicaludemann67@gmail.com>
2022-07-29 10:43:43 -0300

    solucionando prob edicion

--- src/main/java/com/backend/Back/Security/jwt/JwtProvider.java
+++ src/main/java/com/backend/Back/Security/jwt/JwtProvider.java
@@ -10,10 +10,13 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.SignatureException;
 import io.jsonwebtoken.UnsupportedJwtException;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 
 import org.springframework.stereotype.Component;
 
@@ -29,8 +32,8 @@ public class JwtProvider {
     
      public String generateToken(Authentication authentication){
         UsuarioPrincipal usuarioPrincipal = (UsuarioPrincipal) authentication.getPrincipal();
-        //List<String> roles = usuarioPrincipal.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList());
-        return Jwts.builder().setSubject(usuarioPrincipal.getUsername())
+        List<String> roles = usuarioPrincipal.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList());
+        return Jwts.builder().setSubject(usuarioPrincipal.getUsername()).claim("roles", roles)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(new Date().getTime() + expiration * 1000))
                 .signWith(SignatureAlgorithm.HS512, secret)
